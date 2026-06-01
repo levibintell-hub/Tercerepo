5a7e7b8e22e0ab057737c2db1f3c212a7d98ca6d
leviB <levibintell@gmail.com>
2026-06-01 14:46:49 -0600

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Tercerepo',
+    version='0.1',
+    packages=find_packages(),
+    author='leviB',
+    author_email='levibintell@gmail.com',
+    url='https://github.com/levibintell-hub/git-github',
+)
